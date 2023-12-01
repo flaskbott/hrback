8756185f76698a212879c58a604406a0602679a0
flaskbott <929719757@qq.com>
2023-12-01 23:03:15 +0800

    day04

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,12 +1,75 @@
 <template>
   <div class="container">
     <div class="app-container">
-      组织架构
+      <el-tree default-expand-all :data="depts" :props="defaultProps" :expand-on-click-node="false">
+        <!-- 节点结构 -->
+        <!-- v-slot="{ node, data }" 只能作用在template -->
+        <template v-slot="{ data }">
+          <el-row style="width:100%;height:40px" type="flex" justify="space-between" align="middle">
+            <el-col>{{ data.name }}</el-col>
+            <el-col :span="4">
+              <span class="tree-manager">{{ data.managerName }}</span>
+              <!-- $event 实参 表示类型 -->
+              <el-dropdown @command="operateDept">
+                <!-- 显示区域内容 -->
+                <span class="el-dropdown-link">
+                  操作<i class="el-icon-arrow-down el-icon--right" />
+                </span>
+                <!-- 下拉菜单选项 -->
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+                  <add-dept :show-dialog.sync="showDialog" :current-node-id="currentNodeId" @updateDepartment="getDepartment" />
+                  <el-dropdown-item command="edit">编辑部门</el-dropdown-item>
+                  <el-dropdown-item command="del">删除</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </el-row>
+        </template>
+      </el-tree>
     </div>
   </div>
 </template>
 <script>
+import { getDepartment } from '@/api/department'
+import { transListToTreeData } from '@/utils/index'
 export default {
-  name: 'Department'
+  name: 'Department',
+  data() {
+    return {
+      showDialog: false,
+      depts: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  },
+  created() {
+    this.getDepartment() // 调用获取数据的接口
+  },
+  methods: {
+    operateDept(type) {
+      if (type === 'add') {
+        this.showDialog = true
+      }
+    },
+    // 封装好方法
+    async getDepartment() {
+      const result = await getDepartment()
+      this.depts = transListToTreeData(result, 0)
+    }
+  }
 }
 </script>
+<style scoped>
+.app-container {
+  padding: 30px 140px;
+  font-size: 14px;
+}
+.tree-manager {
+  width: 50px;
+  display: inline-block;
+  margin: 10px;
+}
+</style>
